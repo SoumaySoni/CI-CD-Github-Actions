ac535e4de300ace9bf19d3a2fe17fd9b77f0daae
soumay soni <soumaysoni1687@gmail.com>
2026-03-03 15:40:38 +0530

    fix: update success message in root endpoint response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({ message: "Backend deployed successfully 🚀" });
+    res.json({ message: "Backend deployed successfully 🚀 by soumay ci/cd" });
 });
 
 app.listen(PORT, () => {
